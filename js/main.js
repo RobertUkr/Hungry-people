687c3c13cf2e1671b8c3f7e5e5961414fd4c6b73
RobertUkr <pryadchenkor@gmail.com>
2021-05-05 13:51:29 +0300

    commit all edited files

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
 $(function(){
-
+    $('.top__slider').slick({
+        arrows: false,
+        dots: true,
+        autoplay: true,
+    });
+  
+  $('.menu__btn').on('click', function () {
+    $('.menu__item').toggleClass('menu__item--active')
+    
+  });
 });
